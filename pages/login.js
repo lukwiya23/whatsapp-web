55a3cadf7e1ddf68e9978911068a9bd04b86cf70
lukwiya23 <luk23bonnie8@gmail.com>
2021-07-06 21:44:39 +0300

    ||Login Page||

---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function login() {
+    return (
+        <Container>
+            <Head>
+                <title>
+              Login Page
+            </title>
+            </Head>
+        </Container>
+    )
+}
+
+export default login
+
+
+const Container = styled.div``
