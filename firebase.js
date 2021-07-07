3526c07cc055cd31396e3771338944a5b59150a4
lukwiya23 <luk23bonnie8@gmail.com>
2021-07-07 14:53:57 +0300

    ||bug fix in firebase config||

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy8oBhW2KDUDXZDZ3a3GAV_DttwWpLcxI",
-    authDomain: "whatsapp-clone-5ef63.firebaseapp.com",
-    projectId: "whatsapp-clone-5ef63",
-    storageBucket: "whatsapp-clone-5ef63.appspot.com",
-    messagingSenderId: "393786300592",
-    appId: "1:393786300592:web:4c973e1ed1dc87544ac375"
-  };
-
+  apiKey: "AIzaSyDy8oBhW2KDUDXZDZ3a3GAV_DttwWpLcxI",
+  authDomain: "whatsapp-clone-5ef63.firebaseapp.com",
+  projectId: "whatsapp-clone-5ef63",
+  storageBucket: "whatsapp-clone-5ef63.appspot.com",
+  messagingSenderId: "393786300592",
+  appId: "1:393786300592:web:4c973e1ed1dc87544ac375"
+};
   const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig): firebase.app()
 
   const db = app.firestore();
